8e163da34b2da9362528ba6ef6e8720b62afafbc
thecrether@gmail.com <denisimeri6@gmail.com>
2020-08-14 19:58:19 +0200

    added placeholder to key variable (replace when using it)

--- src/ssl_utils.rs
+++ src/ssl_utils.rs
@@ -2,20 +2,19 @@ use openssl::ssl::{SslConnector, SslMethod};
 use std::io::Result;
 use std::io::Write;
 
-const KEY: &'static str = "1234";
-const ID: &'static str = "CoaP";
+const KEY: &'static str = "<placeholder>";
+const ID: &'static str = "Client_identity";
 
 pub fn get_ssl_connector() -> Result<SslConnector> {
-    let mut builder = SslConnector::builder(SslMethod::dtls())?;
+  let mut builder = SslConnector::builder(SslMethod::dtls())?;
 
-    builder.set_psk_client_callback(move |_ssl, _hint, mut identity_buffer, mut psk_buffer| {
-        identity_buffer.write_all(ID.as_bytes()).unwrap();
-        psk_buffer.write_all(KEY.as_bytes()).unwrap();
-        Ok(KEY.len())
-    });
-    builder
-        .set_cipher_list("ECDHE-PSK-AES128-CBC-SHA256:PSK-AES128-CCM8:ECDHE-ECDSA-AES128-CCM8")?;
+  builder.set_psk_client_callback(move |_ssl, _hint, mut identity_buffer, mut psk_buffer| {
+    identity_buffer.write_all(ID.as_bytes()).unwrap();
+    psk_buffer.write_all(KEY.as_bytes()).unwrap();
+    Ok(KEY.len())
+  });
+  builder.set_cipher_list("ECDHE-PSK-AES128-CBC-SHA256:PSK-AES128-CCM8:ECDHE-ECDSA-AES128-CCM8")?;
 
-    let connector = builder.build();
-    return Ok(connector);
+  let connector = builder.build();
+  return Ok(connector);
 }
